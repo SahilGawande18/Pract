f459c2782008375930574e0d069c5b465fa58b92
SahilGawande18 <117385129+SahilGawande18@users.noreply.github.com>
2024-11-05 21:56:46 +0530

    Add files via upload

---
+++ bank.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract Bank
+{
+    int bal;
+    constructor()  
+    {
+        bal=1;
+    }
+    function getBalance() view public returns(int)
+    {
+        return bal;
+    }
+    function withdraw(int amt) public 
+    {
+        if(bal<amt)
+        {
+            bal=0;
+        }
+        else{
+            bal = bal-amt;
+        }
+    }
+    function deposit(int amt) public 
+    {
+        bal = bal+amt;
+    }
+}
